d2fd85d62a481058f8203b4e588c2132775549cc
William Tang <william.tang@infomagnus.com>
2025-09-08 20:01:28 +0900

    Add request dependency and implement /request route to fetch Google homepage

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/request', (req, res) => {
+  request('https://www.google.jp', (error, response, body) => {
+    if (error) {
+      res.status(500).send('Error fetching google.jp');
+    } else {
+      res.send(response.body);
+    }
+  });
 });
 
 app.listen(port, () => {
